6677209ba29c1f024819783170e2a5994aa250ea
Choih0401 <36701345+Choih0401@users.noreply.github.com>
2018-09-20 13:25:13 +0900

    First operator
    first file

---
+++ oper.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include<windows.h>
+
+char *type, *charvar, *name;
+int intvar;
+float floatvar;
+
+void vartype(char arr[]);
+
+int main(int argc, char* argv[]) {
+	char arr1[100];
+	gets(arr1);
+	vartype(arr1);
+	system("pause");
+}
+
+void vartype(char arr[]) {
+	char *ptr = strtok(arr, " ");
+	strcpy(&type, &ptr);
+	printf("%s\n", type);
+	if (ptr != NULL) {
+		ptr = strtok(NULL, "=");
+		strcpy(&name, &ptr);
+		printf("%s\n", name);
+	}
+	else {
+		printf("Error\n");
+	}
+	if (ptr != NULL) {
+		ptr = strtok(NULL, ";");
+		if (!strncmp(type, "int", 3)) {
+			intvar = atoi(ptr);
+			printf("%d\n", intvar);
+		}
+		else if (!strncmp(type, "char", 4)) {
+			strcpy(charvar, ptr);
+			printf("%s\n", charvar);
+		}
+		else if (!strncmp(type, "float", 5)) {
+			floatvar = atof(ptr);
+			printf("%f\n", floatvar);
+		}
+		else {
+			printf("Error\n");
+		}
+	}
+	else {
+		printf("Error\n");
+	}
+}
